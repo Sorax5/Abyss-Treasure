2456934f941ba54fdbebb04ef76c6a50ba151062
1ZFly1 <pecnikcyrille@gmail.com>
2022-01-18 12:55:21 +0100

    fix

--- Assets/Script/SubMouvement.cs
+++ Assets/Script/SubMouvement.cs
@@ -6,42 +6,59 @@ using UnityEngine;
 public class SubMouvement : MonoBehaviour
 {
     public float moveSpeed;
-    public float rotationSpeed;
+    public float moveBoost;
+    public bool wait = true;
+    public bool isFacingLeft = true;
 
     public Rigidbody2D rb;
+    public Transform trs;
     private Vector3 velocity = Vector3.zero;
 
     public Animator heliceAnimator;
 
-    // Start is called before the first frame update
-
-
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        Vector2 movementDirection = new Vector2(horizontalMovement, verticalMovement);
-        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
-        movementDirection.Normalize();
+        MovePlayer(horizontalMovement, verticalMovement);
 
-        transform.Translate(movementDirection * moveSpeed * inputMagnitude * Time.deltaTime, Space.World);
-
-        if (movementDirection != Vector2.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
+        Boost();
 
         float characterVelocity = Mathf.Abs(rb.velocity.x);
 
-        heliceAnimator.SetFloat("speed",characterVelocity);
+        heliceAnimator.SetFloat("speed", characterVelocity);
     }
 
-    void MovePlayer (float _horizontalMovement, float _verticalMovement)
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, _verticalMovement);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if(Input.GetKey(KeyCode.LeftArrow) && !isFacingLeft)
+        {
+            trs.transform.localScale = new Vector3(2, trs.transform.localScale.y, trs.transform.localScale.z);
+            isFacingLeft = true;
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow) && isFacingLeft)
+        {
+            trs.transform.localScale = new Vector3(-2, trs.transform.localScale.y, trs.transform.localScale.z);
+            isFacingLeft = false;
+        }
+    }
+
+    void Boost()
+    {
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            float horizontalMovement = Input.GetAxis("Horizontal") * moveBoost * Time.deltaTime*2;
+            float verticalMovement = Input.GetAxis("Vertical") * moveBoost * Time.deltaTime*2;
+
+            Vector3 targetVelocity = new Vector2(horizontalMovement, verticalMovement);
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        }
+
     }
 }
